ba61e8db2884f831a7cafbcbc8b14e08e240ffbf
john <jpnokid@gmail.com>
2023-12-08 21:45:25 +0300

    Revert "initial commit before uploading to github"
    This reverts commit 91f5b1b0a09cd14699064544c74c2300fb96a28d.

--- mjkitchenapi/members/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login-user', views.login_user, name='login'),
-    path('logout-user', views.logout_user, name='logout'),
-    path('register-user', views.register_user, name='register-user')
-]
